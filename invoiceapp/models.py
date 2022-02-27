454dc2e870c2fda12a3ea7cd2d9ba4fefa65bfd7
hosenmdaltaf <altafhmeazee1999@outlook.com>
2022-02-27 12:58:06 +0600

    model complate

--- invoiceapp/models.py
+++ invoiceapp/models.py
@@ -1,17 +1,22 @@
 from unicodedata import name
 from django.db import models
 
-class Invoice(models.Model):
-    name=models.CharField(max_length=100)
-    date =models.DateField(auto_now=True)
-    quantity
-    product_name 
-    price =
-    total_price =
-    sub_total =
-    Payment_method =
-    Shipping_charge =
-    total =
-    notes =models.TextField(null=True,blank=True)
+# class Product(models.Model):
+#     product_name = models.TextField()
+#     date =models.DateField(auto_now=True)
+#     quantity = models.IntegerField()
+#     price = models.IntegerField()
 
+class Invoice(models.Model):
+    client_name = models.CharField(max_length=255)
+    product_name = models.TextField()
+    date =models.DateField(auto_now=True)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    total_price = models.IntegerField()
+    sub_total = models.IntegerField()
+    Payment_method = models.TextField(null=True,blank=True)
+    Shipping_charge = models.IntegerField()
+    total = models.IntegerField()
+    notes =models.TextField(null=True,blank=True)
 
