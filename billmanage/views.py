fa9ffe5d9e9a5957d04372021686a8f375307bba
hosenmdaltaf <altafhmeazee1999@outlook.com>
2022-03-18 17:21:03 +0600

    changes made

--- billmanage/views.py
+++ billmanage/views.py
@@ -7,9 +7,7 @@ from .models import bill, item
 from django.db.models import Count
 
 from django.contrib.auth import authenticate, login,logout
-
 from django.contrib.auth.models import  User
-
 from django.contrib.auth.decorators import login_required
 
 
